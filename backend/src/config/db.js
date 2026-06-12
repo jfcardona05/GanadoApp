583cd8df9477f759a6ac64625f9925be25c13aeb
jfcardona05 <cardonareyj@gmail.com>
2026-06-12 16:20:46 -0500

    añadidos archivos emporales para deploy en railway

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,13 +3,14 @@ require('dotenv').config();
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
+  multipleStatements: true
 });
 
 module.exports = pool;
