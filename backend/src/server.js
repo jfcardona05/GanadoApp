fa88bcd1bc5aec051daa35964cedf01ed668d067
jfcardona05 <cardonareyj@gmail.com>
2026-06-12 15:45:43 -0500

    Preparar GanadoApp para deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,6 @@ require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Servidor de GanadoApp corriendo en http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Servidor de GanadoApp corriendo en el puerto ${PORT}`);
 });
