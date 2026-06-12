ae1af32274fc55f23fcaeb3a6d46173514eaf23a
jfcardona05 <cardonareyj@gmail.com>
2026-06-12 17:37:05 -0500

    c

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+
 
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
   ],
   preview: {
     host: '0.0.0.0',
     allowedHosts: [
+      'adorable-alignment-production-852f.up.railway.app',
       'adorable-alignment-production-153c.up.railway.app',
     ],
   },
